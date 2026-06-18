3ebafd7ce8acf04ec1c5e3a330d0b309ada6f330
LogicHymn <shlok.alpha21@gmail.com>
2026-06-19 04:02:05 +0530

    Day 2: Neon setup, schema created, dummy data seeded, DB connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./db');
 require('dotenv').config();
-
 const app =express();
 
 app.use(cors());
@@ -9,6 +9,16 @@ app.use(express.json());
 
 app.get('/', (req,res) =>{
     res.json({message: 'Server is running'})
+});
+
+app.get('/test-db', async (req,res) => {
+    try{
+        const result = await pool.query('SELECT * FROM products LIMIT 3');
+        res.json(result.rows);
+    } catch (err){
+        console.log(err);
+        res.status(500).json({error: 'Databse connection failed'});
+    }
 });
 
 const PORT = process.env.PORT || 5000;
